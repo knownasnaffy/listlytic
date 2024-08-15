87c8097f0841aa4f8a499afa2f887053e68a9482
Naffy Dharni <naffydharni006@gmail.com>
2024-08-15 12:13:21 +0530

    config prettier and pretify code

--- src/App.tsx
+++ src/App.tsx
@@ -1,67 +1,69 @@
-import { Search } from "lucide-react";
-import { useState, useEffect } from "react";
-import ItemList, { Item } from "./ItemList";
-import { Input } from "./components/ui/input";
-import { Separator } from "./components/ui/separator";
+import { Search } from 'lucide-react'
+import { useState, useEffect } from 'react'
+import ItemList, { Item } from './ItemList'
+import { Input } from './components/ui/input'
+import { Separator } from './components/ui/separator'
 
 function App() {
-  const [items, setItems] = useState<Item[]>([]);
-  const [filteredItems, setFilteredItems] = useState<Item[]>([]);
+  const [items, setItems] = useState<Item[]>([])
+  const [filteredItems, setFilteredItems] = useState<Item[]>([])
 
-  const [loading, setLoading] = useState(true);
-  const [searchQuery, setSearchQuery] = useState("");
+  const [loading, setLoading] = useState(true)
+  const [searchQuery, setSearchQuery] = useState('')
 
   useEffect(() => {
     // Fetch data from the API
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((data) => {
-        setItems(data);
-        setFilteredItems(data); // Initially set the filtered items as all items
-        setTimeout(() => { // Temporary gap for the visibility of the loader in development
-          setLoading(false);
-        }, 1000);
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(data => {
+        setItems(data)
+        setFilteredItems(data) // Initially set the filtered items as all items
+        setTimeout(() => {
+          // Temporary gap for the visibility of the loader in development
+          setLoading(false)
+        }, 1000)
       })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        setLoading(false);
-      });
-  }, []);
+      .catch(error => {
+        console.error('Error fetching data:', error)
+        setLoading(false)
+      })
+  }, [])
 
   useEffect(() => {
     // Filter the items based on the search query
-    const result = items.filter((item) =>
-      item.name.toLowerCase().includes(searchQuery.toLowerCase())
-    );
-    setFilteredItems(result);
-  }, [searchQuery, items]);
+    const result = items.filter(item =>
+      item.name.toLowerCase().includes(searchQuery.toLowerCase()),
+    )
+    setFilteredItems(result)
+  }, [searchQuery, items])
   return (
     <>
-      <div className="h-screen bg-background flex max-md:mt-4 md:items-center justify-center">
-        <div className="p-4 space-y-4">
-          <h1 className="text-2xl font-bold">Who's secret do you need?</h1>
+      <div className='h-screen bg-background flex max-md:mt-4 md:items-center justify-center'>
+        <div className='p-4 space-y-4'>
+          <h1 className='text-2xl font-bold'>Who's secret do you need?</h1>
 
-          <div className="relative">
-            <Search className="h-4 w-4 absolute top-1/2 left-4 stroke-muted-foreground transform -translate-y-1/2" />
+          <div className='relative'>
+            <Search className='h-4 w-4 absolute top-1/2 left-4 stroke-muted-foreground transform -translate-y-1/2' />
             <Input
-              type="text"
+              type='text'
               value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)}
-              placeholder="Search items..."
-              className="pl-10"
+              onChange={e => setSearchQuery(e.target.value)}
+              placeholder='Search items...'
+              className='pl-10'
             />
           </div>
 
           <Separator />
+
           {loading ? (
-            <div className="text-center animate-pulse">Loading...</div>
+            <div className='text-center animate-pulse'>Loading...</div>
           ) : (
             <ItemList items={filteredItems} />
           )}
         </div>
       </div>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/ItemList.tsx
+++ src/ItemList.tsx
@@ -1,29 +1,29 @@
-import { Card } from "./components/ui/card";
-import { User } from "lucide-react";
+import { Card } from './components/ui/card'
+import { User } from 'lucide-react'
 
 export interface Item {
-  id: number;
-  name: string;
+  id: number
+  name: string
 }
 
 const ItemList = ({ items }: { items: Item[] }) => {
   return (
-    <ul className="space-y-2">
+    <ul className='space-y-2'>
       {items.length > 0 ? (
-        items.map((item) => (
+        items.map(item => (
           <li key={item.id}>
-            <Card className="py-2 px-4 font-normal inline-flex w-full gap-2 items-center">
-              <User className="h-4 w-4 " /> {item.name}
+            <Card className='py-2 px-4 font-normal inline-flex w-full gap-2 items-center'>
+              <User className='h-4 w-4 ' /> {item.name}
             </Card>
           </li>
         ))
       ) : (
-        <li className="text-center text-muted-foreground">
-          No one like that over here {">.<"}
+        <li className='text-center text-muted-foreground'>
+          No one like that over here {'>.<'}
         </li>
       )}
     </ul>
-  );
-};
+  )
+}
 
-export default ItemList;
+export default ItemList
