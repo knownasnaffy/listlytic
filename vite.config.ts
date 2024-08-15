bfd5941eb733b3e64dc7b9828c35dca0d77ab56c
Naffy Dharni <naffydharni006@gmail.com>
2024-08-15 12:56:51 +0530

    fix base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: 'listlytic/',
+  base: '/listlytic/',
   plugins: [react()],
   resolve: {
     alias: {
